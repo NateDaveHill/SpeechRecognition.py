dd39e47d38d14536a32a01678de8d282144188e5
NateDaveHill <nathanhill123@gmail.com>
2023-10-23 22:48:08 -0400

    got to set up a speech recognition tool that works

---
+++ main.py
@@ -0,0 +1,30 @@
+from speech_recognition import Microphone, Recognizer, exceptions
+
+def SpeechRecognition():
+
+    print("Listening!")
+
+    speech_recognizer = Recognizer()
+
+    with Microphone() as mic:
+
+        try:
+
+            speech_recognizer.adjust_for_ambient_noise(mic, duration=0.2)
+
+            text = speech_recognizer.recognize_google(speech_recognizer.listen(mic), language="en-US")
+
+            text = str(text).lower()
+
+            return text
+
+        except exceptions.UnknownValueError:
+
+            return ""
+
+        except Exception as e:
+
+            return ""
+
+
+print("You said: " + SpeechRecognition())
